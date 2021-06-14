bec5567bb57b77d3956285c6c55f4dfb647f3957
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-06-14 13:35:29 +0900

    コントローラーの修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   def index
     categories = Category.all
-    render json: categories
+    manuals = Manual.all.order('created_at desc')
+    datas = [categories, manuals]
+    
+    render json: datas
   end
 end
