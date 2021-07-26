db1f04c8e17db064dd3b5d806a6ac5049e4d8178
kata-myu <tatsuya.katagiri@di-v.co.jp>
2021-07-26 10:30:52 +0900

    削除・検索機能

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,63 @@ class CategoriesController < ApplicationController
     
     render json: datas
   end
+
+  def create
+    category = Category.new(category_params)
+    category.save
+
+    categories = Category.all
+    manuals = Manual.all.order('created_at desc')
+    datas = [categories, manuals]
+    
+    render json: datas
+  end
+
+  def create_manual
+    manual = Manual.new(manual_params)
+    manual.save
+
+    categories = Category.all
+    manuals = Manual.all.order('created_at desc')
+    datas = [categories, manuals]
+    
+    render json: datas
+  end
+
+  def delete_manual
+    manual = Manual.find(params[:id])
+    manual.destroy
+
+    categories = Category.all
+    manuals = Manual.all.order('created_at desc')
+    datas = [categories, manuals]
+    
+    render json: datas
+  end
+
+  def select_category
+    manuals = Manual.where(category_id: params[:id])
+    categories = Category.all
+    datas = [categories, manuals]
+    
+    render json: datas
+  end
+
+  def search_manual
+    manuals = Manual.where("job LIKE(?)", "%#{params[:word]}%")
+    categories = Category.all
+    datas = [categories, manuals]
+    
+    render json: datas
+  end
+
+
+  private
+  def category_params
+    params.permit(:name)
+  end
+
+  def manual_params
+    params.permit(:category_id, :job, :heading, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories, only: :index
+  resources :categories do
+    collection do
+      post :create_manual
+      delete :delete_manual
+      get :select_category
+      get :search_manual
+    end
+  end
 end
